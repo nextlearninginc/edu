e4ed8cee3a95b484e32221e3871f30318bf94b41
nextlearninginc <145260764+nextlearninginc@users.noreply.github.com>
2023-09-24 15:08:56 +0900

    구글 애즈 gtag 삽입

--- gtm.js
+++ gtm.js
@@ -3,3 +3,5 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WW93ZCSB');
+
+gtag('config', 'AW-11346387672');
